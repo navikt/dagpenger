9ea64cf7bb93f5cf539b4a244d481b7f4c484ea9
André Roaldseth <andre@roaldseth.net>
2018-07-27 15:15:28 +0200

    Add a dummy implementation of the Joark WS

---
+++ joark-mottak/src/main/kotlin/no/nav/dagpenger/joark/mottak/JournalpostArkivDummy.kt
@@ -0,0 +1,8 @@
+package no.nav.dagpenger.joark.mottak
+
+class JournalpostArkivDummy : JournalpostArkiv {
+    override fun hentInngåendeJournalpost(journalpostId: String): String {
+        return "bar"
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
