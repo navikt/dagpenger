ad074afeb2f9b651d54ba28b2faab8973444c77e
André Roaldseth <andre.roaldseth@nav.no>
2018-10-03 13:13:08 +0200

    Add a template for new services
    To reduce drift between service we need to generate and maintain them in
    a shared way where possible.
    
    This is first just a simple template that can be used as a basis.
    
    But we should look into tools that can continue to maintain and share
    these files where desired after the initial setup

---
+++ .service-template/Dockerfile
@@ -0,0 +1,8 @@
+FROM navikt/java:10
+ARG APP_NAME
+ARG DIST_TAR
+ADD ${DIST_TAR}.tar /app
+RUN mkdir /app/bin \
+  && mv /app/${DIST_TAR}/bin/${APP_NAME} /app/bin/app \
+  && mv /app/${DIST_TAR}/lib /app/lib \
+  && rm -rf /app/${DIST_TAR}
---
+++ .service-template/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    id("application")
+    kotlin("jvm") version "1.2.51"
+    id("com.diffplug.gradle.spotless") version "3.13.0"
+    id("com.palantir.docker") version "0.20.1"
+    id("com.palantir.git-version") version "0.11.0"
+    id("com.adarshr.test-logger") version "1.5.0"
+}
+
+apply {
+    plugin("com.diffplug.gradle.spotless")
+    plugin("com.adarshr.test-logger")
+}
+
+repositories {
+    jcenter()
+    maven(url = "http://packages.confluent.io/maven/")
+    maven(url = "https://repo.adeo.no/repository/maven-releases/")
+}
+
+val gitVersion: groovy.lang.Closure<Any> by extra
+version = gitVersion()
+group = "no.nav.dagpenger"
+
+application {
+    applicationName = "dagpenger-SERVICENAME"
+    mainClassName = "no.nav.dagpenger.SERVICENAME"
+}
+
+docker {
+    name = "repo.adeo.no:5443/navikt/${application.applicationName}"
+    buildArgs(mapOf(
+        "APP_NAME" to application.applicationName,
+        "DIST_TAR" to "${application.applicationName}-${project.version}"
+    ))
+    files(tasks.findByName("distTar")?.outputs)
+    pull(true)
+    tags(project.version.toString())
+}
+
+val kotlinLoggingVersion = "1.4.9"
+val fuelVersion = "1.15.0"
+val confluentVersion = "4.1.2"
+val kafkaVersion = "2.0.0"
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("no.nav.dagpenger:streams:0.1.2")
+    implementation("no.nav.dagpenger:events:0.1.4")
+
+    implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
+    implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
+    implementation("com.github.kittinunf.fuel:fuel-gson:$fuelVersion")
+
+    compile("org.apache.kafka:kafka-clients:$kafkaVersion")
+    compile("org.apache.kafka:kafka-streams:$kafkaVersion")
+    compile("io.confluent:kafka-streams-avro-serde:$confluentVersion")
+
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+    testImplementation("junit:junit:4.12")
+    testImplementation("com.github.tomakehurst:wiremock:2.18.0")
+}
+
+spotless {
+    kotlin {
+        ktlint()
+    }
+    kotlinGradle {
+        target("*.gradle.kts", "additionalScripts/*.gradle.kts")
+        ktlint()
+    }
+}
