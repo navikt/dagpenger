9d14ec8b83ec452f63377782cb195d683a912a9a
André Roaldseth <andre.roaldseth@nav.no>
2018-10-04 13:51:12 +0200

    Add features as an included build as well

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 rootProject.name = "dagpenger-monorepo"
 
-includeBuild("dagpenger-streams")
 includeBuild("dagpenger-events")
+includeBuild("dagpenger-features")
 includeBuild("dagpenger-joark-mottak")
 includeBuild("dagpenger-journalforing-skanning")
+includeBuild("dagpenger-streams")
