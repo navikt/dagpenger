a81b35a4afa96eecccb3712a3c8912f2ea8c5a68
Atle Nordland <atle.nordland@nav.no>
2018-10-04 10:09:29 +0200

    Add project dagpenger-journalforing-skanning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name = "dagpenger-monorepo"
 includeBuild("dagpenger-streams")
 includeBuild("dagpenger-events")
 includeBuild("dagpenger-joark-mottak")
+includeBuild("dagpenger-journalforing-skanning")
