65e0388c44b711c995eaeb841fe5e8651c23970b
André Roaldseth <andre.roaldseth@nav.no>
2018-10-08 12:28:19 +0200

    Rename from features to funksjonelle-tester

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "dagpenger-monorepo"
 
 includeBuild("dagpenger-events")
-includeBuild("dagpenger-features")
+includeBuild("dagpenger-funksjonelle-tester")
 includeBuild("dagpenger-joark-mottak")
 includeBuild("dagpenger-journalforing-skanning")
 includeBuild("dagpenger-journalforing-ruting")
