bfc43815893d80cac231dadc5b12e9339cf0938c
Geir André Lund <geir.andre.lund@nav.no>
2019-01-16 07:59:10 +0100

    Up streams to 0.2.4-SNAPSHOT - Not optional.. we need to figure out how to handle dependencies like this better.

--- .service-template/build.gradle.kts
+++ .service-template/build.gradle.kts
@@ -41,7 +41,7 @@ val kafkaVersion = "2.0.0"
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("no.nav.dagpenger:streams:0.2.2-SNAPSHOT")
+    implementation("no.nav.dagpenger:streams:0.2.4-SNAPSHOT")
     implementation("no.nav.dagpenger:events:0.1.9-SNAPSHOT")
 
     implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
