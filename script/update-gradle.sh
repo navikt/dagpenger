1af8b472ba79afdca624f6cd1f9c989ae45a7ef8
André Roaldseth <andre.roaldseth@nav.no>
2019-06-26 21:17:41 +0200

    Bump Gradle wrapper

--- script/update-gradle.sh
+++ script/update-gradle.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+echo Gradle version to upgrade to?
+read GRADLE_VERSION
 
 ./gradlew wrapper --gradle-version=$GRADLE_VERSION --distribution-type=all && ./gradlew -v
 
-for i in ./*/; do (print $i && cd $i && ./gradlew wrapper --gradle-version=$GRADLE_VERSION --distribution-type=all && ./gradlew -v | grep "Gradle"); done
+for i in ./*/; do (
+        echo $i &&  \
+        cd $i && \
+        test -f ./gradlew && \
+        ./gradlew wrapper \
+                --gradle-version=$GRADLE_VERSION \
+                --distribution-type=all \
+        && ./gradlew -v | grep "Gradle");
+done
