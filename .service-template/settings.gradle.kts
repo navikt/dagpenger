bb9b5786f8427acdb40c2fdbe1430490dd61f099
André Roaldseth <andre.roaldseth@nav.no>
2020-04-16 10:57:14 +0200

    Checkout .service-template from its own repository

--- .service-template/settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "dagpenger-SERVICENAME"
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        jcenter()
-        maven(url = "https://dl.bintray.com/gradle/gradle-plugins")
-    }
-}
